0ed73e60f7b69155f6c759e0bd7d03163eaabe98
TechnoStudy <info@techno.study>
2020-04-27 19:53:54 -0400

    day 27

---
+++ src/day27/task/Task2.java
@@ -0,0 +1,20 @@
+package day27.task;
+
+// create class Student with fields:
+// string name, double examResult
+
+class Student {
+    String name;
+    double examResult;
+}
+
+
+public class Task2 {
+    // create 3 objects of Student class and print them
+    // 1. way create three objects in main
+
+    // 2. way create objects in method
+
+    // 3. way store them in ArrayList
+
+}
