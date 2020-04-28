6cb7f01ec568f4edd6eedc029d6bb96b94107dad
TechnoStudy <info@techno.study>
2020-04-27 20:18:42 -0400

    day 27: Task2 1.way

--- src/day27/task/Task2.java
+++ src/day27/task/Task2.java
@@ -12,6 +12,24 @@ class Student {
 public class Task2 {
     // create 3 objects of Student class and print them
     // 1. way create three objects in main
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "Armin";
+        student1.examResult = 98.7;
+        System.out.println(student1.name + ": " + student1.examResult);
+
+        Student student2 = new Student();
+        student2.name = "Mikasa";
+        student2.examResult = 85.5;
+        System.out.println(student2.name + ": " + student2.examResult);
+
+        Student student3 = new Student();
+        student3.name = "Jon Snow";
+        student3.examResult = 53.5;
+        System.out.println(student3.name + ": " + student3.examResult);
+
+    }
+
 
     // 2. way create objects in method
 
