84fa06a7ed14fb2db343c1f5438aeedb2af60c2e
TechnoStudy <info@techno.study>
2020-04-27 20:55:46 -0400

    day27 Task2: 3.way

--- src/day27/task/Task2.java
+++ src/day27/task/Task2.java
@@ -3,6 +3,8 @@ package day27.task;
 // create class Student with fields:
 // string name, double examResult
 
+import java.util.ArrayList;
+
 class Student {
     String name;
     double examResult;
@@ -28,19 +30,32 @@ public class Task2 {
 //        student3.examResult = 53.5;
 //        System.out.println(student3.name + ": " + student3.examResult);
 
-        Student student1 = createStudent("Armin", 98.8);
-        printStudent(student1);
+//        Student student1 = createStudent("Armin", 98.8);
+//        printStudent(student1);
 
-        Student student2 = createStudent("Mikasa", 85.5);
-        printStudent(student2);
+//        Student student2 = createStudent("Mikasa", 85.5);
+//        printStudent(student2);
 
-        Student student3 = createStudent("Jon Snow", 53.5);
-        printStudent(student3);
+//        Student student3 = createStudent("Jon Snow", 53.5);
+//        printStudent(student3);
+
+        // 3. way store them in ArrayList, and print Students using loop
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(createStudent("Armin", 98.8));
+        students.add(createStudent("Mikasa", 85.5));
+        students.add(createStudent("Jon Snow", 53.5));
+        students.add(createStudent("Dogan", 100));
+        students.add(createStudent("Dos", -0.1));
+
+
+        for(Student student : students) {
+            printStudent(student);
+        }
+
 
     }
 
     // 2. way create objects in method
-    // 8:30
     public static Student createStudent(String name, double examResult) {
         // your code to create Student object and return it;
         Student student = new Student();
@@ -55,6 +70,5 @@ public class Task2 {
         System.out.println(student.name + " -> " + student.examResult);
     }
 
-    // 3. way store them in ArrayList
 
 }
