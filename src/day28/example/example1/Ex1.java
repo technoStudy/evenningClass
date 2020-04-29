658184a6490ffc38df1c8ea7cb07a9c89e80151b
TechnoStudy <info@techno.study>
2020-04-28 21:11:32 -0400

    day28 TASK 2

--- src/day28/example/example1/Ex1.java
+++ src/day28/example/example1/Ex1.java
@@ -1,9 +1,9 @@
 package day28.example.example1;
 
 class Person {
+
     /*
-    A variable which is created inside the class but outside the method is known
-    as an instance variable.
+    A variable which is created inside the class but outside the method is known as an instance variable.
     Instance variable doesn't get memory at compile time.
     It gets memory at runtime when an object or instance is created.
     That is why it is known as an instance variable.
@@ -19,13 +19,17 @@ class Person {
     }
 
     String getInitials() {
-        return name.charAt(0) + "." + surname + ".";
+        return name.charAt(0) + "." + surname.charAt(0) + ".";
     }
 
     void print() {
+        System.out.println("\n-----------------------------------");
         System.out.println("Name: " + name);
         System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Birth Year: " + getBirthYear());
         System.out.println("Initials: " + getInitials());
+        System.out.println("-----------------------------------");
     }
 }
 
@@ -36,20 +40,20 @@ public class Ex1 {
     public static void main(String[] args) {
         Person person1 = new Person();
         person1.name = "Mark";
+        person1.surname = "Zuckerberg";
         person1.age = 25;
         int person1BirthYear = person1.getBirthYear();
+        String person1Initials = person1.getInitials();
+        person1.print();
 
         Person person2 = new Person();
-        person2.name = "Dos";
+        person2.name = "Tony";
+        person2.surname = "Stark";
         person2.age = 31;
         int person2BirthYear = person2.getBirthYear();
+        String person2Initials = person2.getInitials();
+        person2.print();
 
-//        int birthYear = getBirthYear(person1);
-
-    }
-
-    public static int getBirthYear(Person person) {
-        return 2020 - person.age;
     }
 
 }
