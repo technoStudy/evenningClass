290dd5eb311270422a296cd7270169162599d2ac
TechnoStudy <info@techno.study>
2020-04-28 20:23:23 -0400

    day28 Ex1

---
+++ src/day28/example/example1/Ex1.java
@@ -0,0 +1,44 @@
+package day28.example.example1;
+
+class Person {
+    /*
+    A variable which is created inside the class but outside the method is known
+    as an instance variable.
+    Instance variable doesn't get memory at compile time.
+    It gets memory at runtime when an object or instance is created.
+    That is why it is known as an instance variable.
+     */
+    String name;
+    int age;
+
+    // when you have methods that are not static
+    // they are instance methods
+    int getBirthYear(){
+        return 2020 - age;
+    }
+}
+
+public class Ex1 {
+    // create a program that calculates
+    // birth year of given person
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Mark";
+        person1.age = 25;
+        int person1BirthYear = person1.getBirthYear();
+
+        Person person2 = new Person();
+        person2.name = "Dos";
+        person2.age = 31;
+        int person2BirthYear = person2.getBirthYear();
+
+//        int birthYear = getBirthYear(person1);
+
+    }
+
+    public static int getBirthYear(Person person) {
+        return 2020 - person.age;
+    }
+
+}
---
+++ src/day28/example/example2/Ex2.java
@@ -0,0 +1,13 @@
+package day28.example.example2;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        String a = "Hi";
+        String b = "Hello";
+
+        int lengthA = a.length(); // 2
+        int lengthB = b.length(); // 5
+
+    }
+}
