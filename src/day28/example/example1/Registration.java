4f0902dbe9b3552e418148e892fcafc845c8587b
TechnoStudy <info@techno.study>
2020-04-28 20:27:49 -0400

    day28 Ex1

---
+++ src/day28/example/example1/Registration.java
@@ -0,0 +1,15 @@
+package day28.example.example1;
+
+public class Registration{
+
+    public static void main(String[] args) {
+        Person john = new Person();
+        john.name = "John";
+        john.age = 18;
+        int birthYear = john.getBirthYear();
+
+
+    }
+
+
+}
