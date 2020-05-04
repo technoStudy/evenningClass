d0e3ac76208716834d7dd31694e0dbd3705f462b
TechnoStudy <info@techno.study>
2020-05-04 19:50:08 -0400

    Java Day 30 this keyword

--- src/day30/example/example2/Car.java
+++ src/day30/example/example2/Car.java
@@ -3,8 +3,14 @@ package day30.example.example2;
 public class Car {
     String model;
 
+    public Car(String model) {
+        this.model = model;
+    }
+
     public void print() {
         String model = "Ford";
         System.out.println("My car model is: " + model);
+        System.out.println("but actually it is: " + this.model);
     }
+
 }
--- src/day30/example/example2/JavaThisKeyword.java
+++ src/day30/example/example2/JavaThisKeyword.java
@@ -4,8 +4,7 @@ package day30.example.example2;
 public class JavaThisKeyword {
 
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.model = "Toyota";
+        Car car1 = new Car("Toyota");
         car1.print();
     }
 }
