513be4d70523a1168b50fba3fe592e40c47c6c30
TechnoStudy <info@techno.study>
2020-05-04 19:14:47 -0400

    Java Day 30 Constructors

---
+++ src/day30/JavaConstructors.java
@@ -0,0 +1,32 @@
+package day30;
+
+class Student {
+    String name;
+    String surname;
+
+    //constructor - special method that returns instance
+    //default constructor, without any parameters
+    public Student() {
+        System.out.println("Default Constructor");
+    }
+
+    //overloaded constructor
+    public Student(String n, String s) {
+        name = n;
+        surname = s;
+    }
+
+}
+
+public class JavaConstructors {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "John";
+        student1.surname = "Wick";
+
+        Student student2 = new Student("Aria", "Stark");
+
+    }
+
+}
