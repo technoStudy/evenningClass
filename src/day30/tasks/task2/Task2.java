71b3ba4b20d174f48297068a937993482f4975ad
TechnoStudy <info@techno.study>
2020-05-04 19:52:32 -0400

    Java Day 30 Task 2

---
+++ src/day30/tasks/task2/Task2.java
@@ -0,0 +1,12 @@
+package day30.tasks.task2;
+
+
+//create a class Bank, in different file
+// add at least 3 attributes
+// add at least 3 constructors
+// add method that return all attributes as one string
+
+// in Task2 class create at least 4 object of Bank class
+// call method to get attributes and print it
+public class Task2 {
+}
