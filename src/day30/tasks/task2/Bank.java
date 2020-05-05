a0bfba69b381bdea577c919bca3f65d92d75207e
TechnoStudy <info@techno.study>
2020-05-04 21:12:39 -0400

    Java Day 30 Task 2

--- src/day30/tasks/task2/Bank.java
+++ src/day30/tasks/task2/Bank.java
@@ -7,8 +7,6 @@ package day30.tasks.task2;
 public class Bank {
     String name;
     int serial;
-    String address;
-
     boolean isClosed;
 
     //1. Constructor: default -> no parameters
@@ -17,18 +15,16 @@ public class Bank {
 
     }
 
+
     //2. Constructor: with 2 parameters
     Bank(String name, int serial) {
         this.name = name;
         this.serial = serial;
-        this.address = "Not defined";
-        this.isClosed = true;
     }
 
     //3.  Constructor: with 3 parameters
     Bank(String name, int serial, boolean isClosed) {
-        this.name = name;
-        this.serial = serial;
+        this(name, serial);
         this.isClosed = isClosed;
     }
 
@@ -38,7 +34,6 @@ public class Bank {
                 "\nname: '" + name + '\'' +
                 "\nserial: " + serial +
                 "\nisClosed: " + isClosed +
-                "\naddress: " + address +
                 "\n----------------------------";
     }
 }
