2074e19727f5caa75af193a04858dbac58c4ed99
TechnoStudy <info@techno.study>
2020-05-05 20:09:24 -0400

    Java Day 31 Task 2

--- src/day31/instanceModifiers/privateModifier/Ex1.java
+++ src/day31/instanceModifiers/privateModifier/Ex1.java
@@ -5,5 +5,8 @@ public class Ex1 {
     public static void main(String[] args) {
         Math.random();
         Math.min(10, 5);
+
+        //can not create Math instance, because its private
+        //Math math = new Math();
     }
 }
