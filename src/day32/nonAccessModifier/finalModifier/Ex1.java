f0e4df689ef7a97c05176c0a132143ef72181af8
TechnoStudy <info@techno.study>
2020-05-07 19:30:46 -0400

    Day 32 final modifier

--- src/day32/nonAccessModifier/finalModifier/Ex1.java
+++ src/day32/nonAccessModifier/finalModifier/Ex1.java
@@ -5,20 +5,18 @@ class Person {
     public String name;
     public final String dob;//date of birth
     public final String eyeColor;
-    public final long fingerprintCode;
 
-    public Person(String name, String dob, String color, long fingerprintCode) {
+    public Person(String name, String dob, String color) {
         this.name = name;
         this.dob = dob;
         this.eyeColor = color;
-        this.fingerprintCode = fingerprintCode;
     }
 }
 
 public class Ex1 {
 
     public static void main(String[] args) {
-        Person person1 = new Person("Halit", "04/07/95", "blue", 1929394);
+        Person person1 = new Person("Halit", "04/07/95", "blue");
 
         person1.name = "Serdar";
 
--- src/day32/nonAccessModifier/finalModifier/School.java
+++ src/day32/nonAccessModifier/finalModifier/School.java
@@ -1,4 +1,16 @@
 package day32.nonAccessModifier.finalModifier;
 
+import java.util.ArrayList;
+
 public class School {
+
+    private final ArrayList<Person> students = new ArrayList<>();
+
+    public static void main(String[] args) {
+        School school = new School();
+        school.students.add(new Person("Max", "01/01/90","Red"));
+        school.students.add(new Person("Aliya", "05/05/89","Gray"));
+
+    }
+
 }
