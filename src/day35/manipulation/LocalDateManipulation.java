8e1675c0de0d712f220d98312aee702015bf483d
TechnoStudy <info@techno.study>
2020-05-11 20:43:20 -0400

    Day 35 date manipulation

--- src/day35/manipulation/LocalDateManipulation.java
+++ src/day35/manipulation/LocalDateManipulation.java
@@ -1,19 +1,30 @@
 package day35.manipulation;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.time.LocalDate;
 import java.time.Month;
 
 public class LocalDateManipulation {
 
-    public static void main(String[] args) {
+    @Test
+    public void testPlus() {
         LocalDate date1 = LocalDate.of(2020, Month.APRIL, 10);
+        LocalDate actual = date1.plusYears(1).plusMonths(1).plusDays(1);
+        System.out.println(actual);
 
-        date1.plusYears(1);
-        date1.plusMonths(1);
-        date1.plusDays(1);
+        String expected = "2021-05-11";
+        Assert.assertEquals(expected, actual.toString());
+    }
 
-        System.out.println(date1);
+    @Test
+    public void testMinus(){
+        LocalDate date = LocalDate.of(2020, Month.APRIL, 10);
+        LocalDate actual = date.minusYears(1).minusMonths(1).minusDays(1);
 
+        String expected = "2019-03-09";
+        Assert.assertEquals(expected, actual.toString());
     }
 
 }
