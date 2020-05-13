0234d4e46f41f420b549b74be908765c19564dc9
TechnoStudy <info@techno.study>
2020-05-12 20:45:27 -0400

    Day 36 Task 2

--- src/day36/tasks/task2/Application.java
+++ src/day36/tasks/task2/Application.java
@@ -1,11 +1,17 @@
 package day36.tasks.task2;
 
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
 
-        Registration regUser = new Registration();
-        regUser.register();
+        Registration examRegistration = new Registration();
+        ArrayList<User> users = examRegistration.register();
+
+        for(User user : users) {
+            System.out.println(user);
+        }
 
     }
 }
--- src/day36/tasks/task2/Registration.java
+++ src/day36/tasks/task2/Registration.java
@@ -1,11 +1,13 @@
 package day36.tasks.task2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //2. create class Registration, use Scanner to create users
 public class Registration {
 
-    public void register() {
+    public ArrayList<User> register() {
+        ArrayList<User> userList = new ArrayList<>();
         System.out.println("Provide name to register:");
 
         Scanner scanner = new Scanner(System.in);
@@ -13,7 +15,13 @@ public class Registration {
         //3. add as many users as you want until
         // you enter word "exit"(Hint: loop, arrayList)
         String text = scanner.nextLine();
-        User user = new User(text);
-        System.out.println(user);
+        while(!text.equalsIgnoreCase("exit")) {
+            User user = new User(text);
+            userList.add(user);
+
+            text = scanner.nextLine();
+        }
+
+        return userList;
     }
 }
