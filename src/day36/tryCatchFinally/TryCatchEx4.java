4427ffd231ba24fb6d6ef9f362c3a8ce9a7494f2
TechnoStudy <info@techno.study>
2020-05-12 21:36:19 -0400

    Day 36 Try Catch Finally

---
+++ src/day36/tryCatchFinally/TryCatchEx4.java
@@ -0,0 +1,23 @@
+package day36.tryCatchFinally;
+
+public class TryCatchEx4 {
+
+    public static void main(String[] args) {
+
+        //real world example
+        try{
+            //lear java
+        }catch(Exception ex){
+            //hard time to understand
+            //topics fast
+            //no time practice
+            //difficult replit questions
+            //hard time to catch the teacher
+        }finally {
+            //I'm gonna be a SDET in 6 month
+            //I'm gonna be a QA engineer in 6 month
+            //I want more than $100.000 salary
+        }
+    }
+
+}
