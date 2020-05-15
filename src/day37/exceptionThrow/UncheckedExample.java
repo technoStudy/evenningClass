a73b81c11f30d2ce713539c236fe41015ad51f78
TechnoStudy <info@techno.study>
2020-05-14 20:10:58 -0400

    Day 37 Checked and Unchecked

--- src/day37/exceptionThrow/UncheckedExample.java
+++ src/day37/exceptionThrow/UncheckedExample.java
@@ -1,4 +1,28 @@
 package day37.exceptionThrow;
 
+
+//Unchecked/Runtime exception
 public class UncheckedExample {
+
+    ///username should have at least 5 characters
+    public static void checkUsername(String username) throws RuntimeException {
+        if(username == null) {
+            throw new NullPointerException("username doesnt exist, it is null");
+        }
+
+        if(username.length() < 5) {
+            throw new IllegalArgumentException("username is too short, please provide at least 6 characters");
+            //throw new RuntimeException("username is too short, please provide at least 6 characters");
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = "dos";
+
+        checkUsername(name);
+
+        System.out.println("You logged in");
+    }
+
+
 }
