9ac6983b2a7abcc37b09341282a5230a43895fee
TechnoStudy <info@techno.study>
2020-05-14 21:45:31 -0400

    Day 37 Finally

--- src/ex.java
+++ src/ex.java
@@ -1,16 +1,20 @@
 public class ex {
     public static void main(String[] args) {
-        int a = 0111;
-        int e = 01_1;
-
-        System.out.println(e);
+        System.out.println(5 / 0);
     }
 
-    public void run(){
-        throw new RuntimeException("me");
-    }
+//    public static int method1() {
+//        int a = 0, b = 0;
+//        try {
+//            return a / b;
+//        } catch(RuntimeException e) {
+//            return -1;
+//        } catch(ArithmeticException e) {
+//            return 0;
+//        } finally {
+//            System.out.print("done");
+//        }
+//    }
 
-    public void run2() throws Exception {
-        throw new Exception("me");
-    }
+
 }
