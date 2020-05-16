6065651acf1aa9f22859a673f823274470eb3814
TechnoStudy <info@techno.study>
2020-05-15 20:48:14 -0400

    Day 38 double division

---
+++ src/day38/tasks/DoubleDivision.java
@@ -0,0 +1,10 @@
+package day38.tasks;
+
+public class DoubleDivision {
+
+    public static void main(String[] args) {
+        System.out.println("0.0/0.0 = " + 0.0 / 0.0);
+        System.out.println("3.3/0.0 = " + 3.3 / 0.0);
+        System.out.println("0.0/2.2 = " + 0.0 / 2.2);
+    }
+}
