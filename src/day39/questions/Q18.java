33ef0c403ead5333dfb6324aa215c9e0bd005c67
TechnoStudy <info@techno.study>
2020-05-18 13:44:56 -0400

    Day 39 interview questions

---
+++ src/day39/questions/Q18.java
@@ -0,0 +1,13 @@
+package day39.questions;
+
+public class Q18 {
+
+    public String _;
+    public String $;
+    public double $200_000_000_000 = 200_000_000_000.0;
+    public int дддв;
+
+
+    public String technoStudyCourse;
+
+}
---
+++ src/day39/questions/Q2.java
@@ -0,0 +1,14 @@
+package day39.questions;
+
+public class Q2 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void main() {
+
+    }
+
+}
