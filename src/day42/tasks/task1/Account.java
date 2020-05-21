db7385066d30a7986e99d41c7fbb453409ec6afc
TechnoStudy <info@techno.study>
2020-05-21 19:12:51 -0400

    Day 39 Task 1

---
+++ src/day42/tasks/task1/Account.java
@@ -0,0 +1,9 @@
+package day42.tasks.task1;
+
+/*
+1. create class Account with public fields: number, balance (do not create anything extra yet)
+ */
+public class Account {
+    public String number;
+    public double balance;
+}
---
+++ src/day42/tasks/task1/Hacker.java
@@ -0,0 +1,15 @@
+package day42.tasks.task1;
+
+//create class Hacker and create instances of Account class
+public class Hacker {
+
+    public static void main(String[] args) {
+//        Account account1 = new Account();
+//        account1.number = "USD43654038649";
+//        account1.balance = 1000;
+//
+//        Account account2 = new Account();
+//        account2.number = "EU12358749534";
+//        account2.balance = 500;
+    }
+}
