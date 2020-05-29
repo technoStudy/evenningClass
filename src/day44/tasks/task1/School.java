b57b9dbef83502e9f25b2209ca5940c66ec60e85
TechnoStudy <info@techno.study>
2020-05-28 21:15:41 -0400

    Day 44 Task 1: part 3

--- src/day44/tasks/task1/School.java
+++ src/day44/tasks/task1/School.java
@@ -1,4 +1,28 @@
 package day44.tasks.task1;
 
+import java.util.List;
+
 public class School {
+    private String name;
+    private int maxStudents;
+    private List<Student> students;
+    private List<Employee> employees;
+
+    public List<Student> registerStudent(Student student){
+        return null;
+    }
+
+    public List<Employee> registerEmployee(Employee employee){
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", maxStudents=" + maxStudents +
+                ", students=" + students +
+                ", employees=" + employees +
+                '}';
+    }
 }
