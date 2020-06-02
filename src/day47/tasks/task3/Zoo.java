d73c08bf6933bc9496e34572001abf4851183404
TechnoStudy <info@techno.study>
2020-06-01 21:47:24 -0400

    Day 47 Task 3

--- src/day47/tasks/task3/Zoo.java
+++ src/day47/tasks/task3/Zoo.java
@@ -1,11 +1,27 @@
 package day47.tasks.task3;
 
+import day47.tasks.task3.bird.Swallow;
+import day47.tasks.task3.cat.Lion;
+import day47.tasks.task3.cat.Tiger;
+import day47.tasks.task3.fish.Salmon;
+
 public class Zoo {
 
     public static void main(String[] args) {
-        //Tiger
-        //Lion
-        //Salmon
-        //Swallow
+        Tiger tiger = new Tiger();
+        tiger.setName("Shere Khan");
+        System.out.println(tiger);
+
+        Lion lion = new Lion();
+        lion.setName("Simba");
+        System.out.println(lion);
+
+        Salmon salmon = new Salmon();
+        salmon.setName("Nemo");
+        System.out.println(salmon);
+
+        Swallow swallow = new Swallow();
+        swallow.setName("Pidgeot");
+        System.out.println(swallow);
     }
 }
