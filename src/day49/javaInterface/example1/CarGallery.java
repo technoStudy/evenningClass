e9cffb890a0fef57746ebfa6efc03f8c3ac39978
TechnoStudy <info@techno.study>
2020-06-04 20:10:05 -0400

    Day 49 interface

--- src/day49/javaInterface/example1/CarGallery.java
+++ src/day49/javaInterface/example1/CarGallery.java
@@ -3,9 +3,25 @@ package day49.javaInterface.example1;
 public class CarGallery {
 
     public static void main(String[] args) {
+        option1();
+    }
+
+    public static void option1() {
         FordMustang mustang = new FordMustang();
+        TeslaCar tesla = new TeslaCar();
+        ToyotaPrius toyota = new ToyotaPrius();
+    }
 
-        
+    public static void option2() {
+        AbstractVehicle mustang = new FordMustang();
+        AbstractVehicle tesla = new TeslaCar();
+        AbstractVehicle toyota = new ToyotaPrius();
+    }
 
+    public static void option3() {
+        Gas mustang = new FordMustang();
+        Electric tesla = new TeslaCar();
+        Electric toyota = new ToyotaPrius();
+        Gas toyota2 = new ToyotaPrius();
     }
 }
