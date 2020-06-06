1de3f0416c6b65fa15b0a2d888cfa8511740a44c
TechnoStudy <info@techno.study>
2020-06-05 21:12:22 -0400

    Day 50 Task 2: Part 2

--- src/day50/tasks/task2/Kitchen.java
+++ src/day50/tasks/task2/Kitchen.java
@@ -1,20 +1,32 @@
 package day50.tasks.task2;
 
-import day50.tasks.task2.food.AdanaKebab;
-import day50.tasks.task2.food.Food;
+import day50.tasks.task2.food.*;
 
 public class Kitchen {
 
     public void cook(Food food) {
-        System.out.println("starting");
+        System.out.println("star cooking");
 
         if(food instanceof AdanaKebab) {
             AdanaKebab kebab = (AdanaKebab) food;
             System.out.println(kebab.marinade());
             System.out.println(kebab.grill());
+        } else if(food instanceof Borsh) {
+            Borsh borsh = (Borsh) food;
+            System.out.println(borsh.boil());
+        } else if(food instanceof Lahmacun) {
+            Lahmacun lahmacun = (Lahmacun) food;
+            System.out.println(lahmacun.dough());
+            System.out.println(lahmacun.topping());
+            System.out.println(lahmacun.bake());
+        } else if(food instanceof Palov) {
+            Palov palov = (Palov) food;
+            System.out.println(palov.fry());
+            System.out.println(palov.boil());
         }
 
 
-        System.out.println("finished");
+        System.out.println(food.taste());
+        System.out.println("serve food");
     }
 }
