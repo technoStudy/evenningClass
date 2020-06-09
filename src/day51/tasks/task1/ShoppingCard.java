7b365b08d103704ced58870e61eb08e59a2c8489
TechnoStudy <info@techno.study>
2020-06-08 21:26:31 -0400

    Day 51 Task 1: Part 3

---
+++ src/day51/tasks/task1/ShoppingCard.java
@@ -0,0 +1,35 @@
+package day51.tasks.task1;
+
+import day51.tasks.task1.device.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCard {
+    private final List<Product> productList = new ArrayList<>();
+
+    public void addProduct(Product product){
+        productList.add(product);
+    }
+
+    public void removeProduct(Product product){
+        productList.remove(product);
+    }
+
+    public double getTotalPrice(){
+        int sum = 0;
+        for(Product product : productList) {
+            sum += product.getPrice();
+        }
+        return sum;
+    }
+
+
+    public void checkOut(double money){
+        if(money >= getTotalPrice()){
+            System.out.println("Your order is confirmed, and shipped to your address");
+        }else {
+            throw new RuntimeException("money is not enough");
+        }
+    }
+}
