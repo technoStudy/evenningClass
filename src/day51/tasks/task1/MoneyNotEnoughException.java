901e57907b71625e3bc7981c2d3223d1e9de7bc1
TechnoStudy <info@techno.study>
2020-06-09 18:42:17 -0400

    Day 51 Task 1: Part 3

--- src/day51/tasks/task1/MoneyNotEnoughException.java
+++
@@ -1,9 +0,0 @@
-package day51.tasks.task1;
-
-public class MoneyNotEnoughException extends RuntimeException {
-
-    public MoneyNotEnoughException(String str){
-        super(str);
-
-    }
-}
--- src/day51/tasks/task1/ShoppingCard.java
+++ src/day51/tasks/task1/ShoppingCard.java
@@ -26,27 +26,7 @@ public class ShoppingCard {
 
 
     public void checkOut(double money) {
-        if(money >= getTotalPrice()) {
-            System.out.println("Your order is confirmed, and shipped to your address");
-        } else {
-            throw new MoneyNotEnoughException("money is not enough");
-        }
+
     }
 
-
-    public void printList() {
-        for(Product product : productList) {
-            //1. way
-            //AbstractDevice p = (AbstractDevice) product;
-            //p.getModel();
-
-            //2. way downcast in default method in Product
-            //product.getModel();
-
-            //3. way create abstract method and implement it in derived class
-
-            //model + price
-            System.out.println(product.getModel() + ": $" + product.getPrice());
-        }
-    }
 }
