d834baaa6bfdfbb199c7c11a622250f0d56f0301
TechnoStudy <info@techno.study>
2020-06-09 21:13:26 -0400

    Day 52 Task: Part 1

---
+++ src/day52/SimpleStringSet.java
@@ -0,0 +1,38 @@
+package day52;
+
+public class SimpleStringSet implements StringSet {
+    //1. way Set
+    //2. way List
+
+    //3. way
+    // Node {
+    //   String val;
+    //   Node next;
+    // }
+
+
+    @Override
+    public void add(String s) {
+
+    }
+
+    @Override
+    public void remove(String s) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/day52/StringSet.java
@@ -0,0 +1,14 @@
+package day52;
+
+public interface StringSet {
+
+    void add(String s);
+
+    void remove(String s);
+
+    int size();
+
+    void clear(); //remove all String from StringSet
+
+    boolean isEmpty();
+}
